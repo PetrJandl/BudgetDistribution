74afe060aae6f022110640658e6d3b60e1193ae9
PetrJandl <petr.jandl@gmail.com>
2021-06-17 13:43:17 +0200

    Test on server2.

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <meta name="recapcha-site-key" content="{{ config('services.recaptcha.secret')}}">
+    <meta name="recapcha-site-key" content="{{ config('services.recaptcha.site.key')}}">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}" >
     <script src="{{ asset('js/app.js') }}" defer></script>
     <title>{{ config('app.name', "Laravel") }}</title>
