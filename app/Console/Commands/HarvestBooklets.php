3d5bb6951e501ee9719ab246203f67091f5f9a2b
PetrJandl <petr.jandl@gmail.com>
2021-06-19 14:31:16 +0200

    Remove error from harvester obalkyknih.cz

--- app/Console/Commands/HarvestBooklets.php
+++ app/Console/Commands/HarvestBooklets.php
@@ -49,10 +49,12 @@ class HarvestBooklets extends Command
             $file = @file_get_contents($ObalkyKnih);
             $data = json_decode($file);
             foreach ($data as $key => $value) {
-                $books[$value->bibinfo->isbn]['item_name'] = $value->bib_title;
+                if (isset($value->bib_title)) {
+		    $books[$value->bibinfo->isbn]['item_name'] = $value->bib_title;
+		}
                 if (isset($value->bib_autor)) {
                     $books[$value->bibinfo->isbn]['item_autor'] = $value->bib_autor;
-                } else {
+                } elseif(isset($value->csn_iso_690)) {
                     $autor = explode("<i>", $value->csn_iso_690);
                     $books[$value->bibinfo->isbn]['item_autor'] = rtrim(ucwords(mb_strtolower(trim($autor[0]), "UTF-8")), ".");
                 }
@@ -71,12 +73,17 @@ class HarvestBooklets extends Command
                 if (isset($value->rating_url)) {
                     $books[$value->bibinfo->isbn]['rating_url'] = $value->rating_url;
                 }
+		if(isset($books[$value->bibinfo->isbn])){
+			Item::where('isbn', '=', $value->bibinfo->isbn)->update($books[$value->bibinfo->isbn]);
+			//echo $value->bibinfo->isbn." OK \n\n";
+		}else{
 
-                Item::where('isbn', '=', $value->bibinfo->isbn)->update($books[$value->bibinfo->isbn]);
+			echo "Kniha ". $value->bibinfo->isbn ." neni na serveru obalkyknih.cz\n";
+		}
             }
-            foreach ($books as $key => $value) {
-                print_r($key);
-            }
+//            foreach ($books as $key => $value) {
+//                print_r($books);
+//            }
         }
         //print_r($books);
         //Item::update($books)->save();
